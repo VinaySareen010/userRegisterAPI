faa5175657f3612277637003ca7f7e92ed9cf7a7
vinay <VinayKhatri010@gmail.com>
2022-06-08 11:46:50 +0530

    commit today

--- Assignment2_userLogin/Controllers/BasicController.cs
+++ Assignment2_userLogin/Controllers/BasicController.cs
@@ -11,22 +11,22 @@ namespace Assignment2_userLogin.Controllers
     [ApiController]
     public class BasicController : ControllerBase
     {
-        public double Add(double num1, double num2)
-        {
-            return num1 + num2;
-        }
-        public double Substract(double num1, double num2)
-        {
-            return num1 - num2;
-        }
-        public double divide(double num1, double num2)
-        {
-            return num1 / num2;
-        }
-        public double Multiply(double num1, double num2)
-        {
-            // To trace error while testing, writing + operator instead of * operator.  
-            return num1 * num2;
-        }
+        //public double Add(double num1, double num2)
+        //{
+        //    return num1 + num2;
+        //}
+        //public double Substract(double num1, double num2)
+        //{
+        //    return num1 - num2;
+        //}
+        //public double divide(double num1, double num2)
+        //{
+        //    return num1 / num2;
+        //}
+        //public double Multiply(double num1, double num2)
+        //{
+            
+        //    return num1 + num2;
+        //}
     }
 }
